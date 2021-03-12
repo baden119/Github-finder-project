0acd298403eac5701de5cafdb3278c967f2e6959
baden allen <baden.j.allen@gmail.com>
2021-03-12 14:03:57 +1100

    More functionality moved out of app.js. everything working.

--- src/components/users/Users.js
+++ src/components/users/Users.js
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { useContext } from 'react'
 import Useritem from './Useritem';
 import Spinner from '../layout/Spinner';
-import PropTypes from 'prop-types';
+import GithubContext from '../../context/github/githubContext';
 
 
-const Users = ({users, loading}) => {
+const Users = () => {
+
+    const githubContext = useContext(GithubContext);
+
+    const { loading, users } = githubContext;
+    
     if(loading){
         return <Spinner />
     } else{
@@ -22,11 +27,6 @@ const userStyle = {
     display: 'grid',
     gridTemplateColumns: 'repeat(3, 1fr)',
     gridGap: '1rem'
-}
-
-Users.propTypes = {
-    users: PropTypes.array.isRequired,
-    loading: PropTypes.bool.isRequired
 }
 
 export default Users
--- src/context/github/GithubState.js
+++ src/context/github/GithubState.js
@@ -12,45 +12,74 @@ import {
 
 
 const GithubState = props => {
-    const initialState = {
-      users: [],
-      user: {},
-      repos: [],
-      loading: false
-    };
+  const initialState = {
+    users: [],
+    user: {},
+    repos: [],
+    loading: false
+  };
+  
+  const [state, dispatch] = useReducer(GithubReducer, initialState);
+
+  // Search Github Users
+  const searchUsers = async text => {
+    setLoading();
+
+    const res = await axios
+    .get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
     
-    const [state, dispatch] = useReducer(GithubReducer, initialState);
+    dispatch({
+        type: SEARCH_USERS,
+        payload: res.data.items
+    });
+  };
 
-    // Search Github Users
-    const searchUsers = async text => {
-        setLoading();
+  // Get info about a single Github user
+  const getUser = async (username) => {
+    setLoading();
+    const res = await axios
+    .get(`https://api.github.com/users/${username}?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    
+    dispatch({
+      type: GET_USER,
+      payload: res.data
+    });
+  };
 
-        const res = await axios
-        .get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-        
-        dispatch({
-            type: SEARCH_USERS,
-            payload: res.data.items
-        });
-    };
+  // Clear users from State
+  const clearUsers = () => dispatch({ type: CLEAR_USERS });
 
-    // Set Loading
-    const setLoading = () => dispatch({ type: SET_LOADING });
+  // Set Loading
+  const setLoading = () => dispatch({ type: SET_LOADING });
 
-    return <GithubContext.Provider
-          value={{
-            users: state.users,
-            user: state.user,
-            repos: state.repos,
-            loading: state.loading,
-            searchUsers
-            // clearUsers,
-            // getUser,
-            // getUserRepos
-          }}
-        >
-        {props.children}
-    </GithubContext.Provider>
+  // Get Users Repos
+  const getUserRepos = async (username) => {
+    setLoading();
+    const res = await axios
+    .get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
+    console.log("beepboop")
+    console.log(res.data)
+    dispatch({
+      type: GET_REPOS,
+      payload: res.data
+    });
+  };
+
+
+  return <GithubContext.Provider
+    value={{
+      users: state.users,
+      user: state.user,
+      repos: state.repos,
+      loading: state.loading,
+      searchUsers,
+      clearUsers,
+      getUser,
+      getUserRepos
+    }}
+    >
+    {props.children}
+  </GithubContext.Provider>
 };
 
 export default GithubState;
